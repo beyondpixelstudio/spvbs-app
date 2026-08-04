782732f8089dd3228fcc5121687345a7ac476f72
Rajesh Kumar Gouda <rajeshkumargouda@Rajeshs-MacBook-Air.local>
2026-08-04 20:22:05 +0530

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const alata = Alata({
   variable: "--font-heading",
@@ -37,6 +38,7 @@ export default function RootLayout({
         <div className="flex-1">{children}</div>
         <Footer />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
